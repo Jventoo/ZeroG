d03e4ba35220d49506c8d984ec35dc7d9788d426
Jack Vento <jackovento@gmail.com>
2020-09-20 18:23:19 -0700

    C++ conversion prep work

---
+++ Source/ZeroG/Public/Game/Player/BaseGameplayPC.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "BaseGameplayPC.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZEROG_API ABaseGameplayPC : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ZeroG/Public/Game/ZGGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "ZGGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZEROG_API UZGGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
