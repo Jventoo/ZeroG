13839ed4ad950614fcc8e4633726c866b41b341e
Jack Vento <jackovento@gmail.com>
2020-10-10 18:20:15 -0700

    Asset organization

--- Source/ZeroG/Private/Pawns/ZeroGCharacter.cpp
+++ Source/ZeroG/Private/Pawns/ZeroGCharacter.cpp
@@ -1,6 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-#include "ZeroGCharacter.h"
+#include "Pawns/ZeroGCharacter.h"
 #include "HeadMountedDisplayFunctionLibrary.h"
 #include "Camera/CameraComponent.h"
 #include "Components/CapsuleComponent.h"
--- Source/ZeroG/ZeroGGameMode.cpp
+++ Source/ZeroG/ZeroGGameMode.cpp
@@ -1,7 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "ZeroGGameMode.h"
-#include "ZeroGCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 AZeroGGameMode::AZeroGGameMode()
